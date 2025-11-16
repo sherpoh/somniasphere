744724a65be117d4def172aeeda20d5116ff2d6e
sherpoh <sherlypontoh20@gmail.com>
2025-11-16 12:11:26 +0700

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,14 @@
+// constants.ts
+
+// Alamat kontrak game FlipCoin
 export const FLIP_GAME_ADDRESS = '0x340DcEaF9bd241B1f6dC6c190c7a53808bcE593A';
 
+// ABI kontrak game FlipCoin
 export const FLIP_GAME_ABI = [
+  // ... ABI untuk playFlip, getLeaderboard, dll
   {
     "inputs": [
-      { "internalType": "uint8", "name": "userChoice", "type": "uint8" }
+      { "internalType": "uint8", "name": "choice", "type": "uint8" }
     ],
     "name": "playFlip",
     "outputs": [],
@@ -18,6 +23,34 @@ export const FLIP_GAME_ABI = [
       { "internalType": "uint256[]", "name": "", "type": "uint256[]" }
     ],
     "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+// Alamat kontrak token FLIP (misalnya ini kontrak ERC20 terpisah)
+export const FLIP_TOKEN_ADDRESS = '0x340DcEaF9bd241B1f6dC6c190c7a53808bcE593A'; // ganti jika beda
+
+// ABI standar ERC20 minimal untuk balance
+export const FLIP_TOKEN_ABI = [
+  {
+    "constant": true,
+    "inputs": [{ "name": "account", "type": "address" }],
+    "name": "balanceOf",
+    "outputs": [{ "name": "", "type": "uint256" }],
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [{ "name": "", "type": "string" }],
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [{ "name": "", "type": "uint8" }],
     "type": "function"
   }
 ];
